e36abacb78cc776d08dc9620d797eb779a78f52e
Outer Cloud <outercloudstudio@gmail.com>
2026-02-03 10:00:12 -0500

    add favicon

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     ['link', { 
       href: 'https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap',
       rel: 'stylesheet'
-    }]
+    }],
+    ['link', { rel: 'icon', href: '/favicon.png' }]
   ]
 })
