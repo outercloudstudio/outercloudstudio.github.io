a8384a2ec91ae91b0a96f76ee22aec1b0612e455
Liam Hanrahan <outercloudstudio@gmail.com>
2026-02-04 15:46:39 -0500

    missing file

---
+++ src/blogs/battlecode-2026/index.md
@@ -0,0 +1,10 @@
+---
+blog: true
+
+title: "Battlecode 2026"
+date: 2026-2-4
+---
+
+Battlecode 2026 Blog
+
+-----
---
+++ src/blogs/blogs.data.ts
@@ -0,0 +1,17 @@
+import { createContentLoader } from 'vitepress'
+export default createContentLoader(
+    'blogs/*/index.md', 
+    {
+        excerpt: true,
+        transform(rawData) {
+            return rawData.sort((a, b) => {
+            return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date)
+        }).map((page) => {
+            return {
+                title: page.frontmatter.title,
+                excerpt: page.excerpt,
+                date: new Date(page.frontmatter.date)
+            }
+        })
+    }
+})
