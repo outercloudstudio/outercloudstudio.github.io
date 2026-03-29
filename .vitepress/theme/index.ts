111fa5a37bdbaf97cd6e6e7737e582d33c9627bd
Liam Hanrahan <outercloudstudio@gmail.com>
2026-03-29 18:37:13 -0400

    improving font and home

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,3 @@
-// https://vitepress.dev/guide/custom-theme
 import Layout from './Layout.vue'
 import type { Theme } from 'vitepress'
 
@@ -9,11 +8,15 @@ import 'vitepress/dist/client/theme-default/styles/vars.css'
 import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
 
 import InlineImage from './components/InlineImage.vue'
+import Emoticon from './components/Emoticon.vue'
+import Quote from './components/Quote.vue'
 
 export default {
 	Layout,
 
 	enhanceApp({ app, router, siteData }) {
 		app.component('InlineImage', InlineImage)
+		app.component('Emoticon', Emoticon)
+		app.component('Quote', Quote)
 	},
 } satisfies Theme
--- .vitepress/theme/views/Home.vue
+++ .vitepress/theme/views/Home.vue
@@ -2,10 +2,39 @@
 
 <template>
     <div class="home">
-		<h1 class="headline">For the <span class="accent-headline">Love</span> of the game</h1>
+		<h1 class="headline">For the <span class="accent">Love</span> of the game</h1>
 
 		<main>
 			<Content />
 		</main>
 	</div>
 </template>
+
+<style scoped>
+.home {
+    position: relative;
+}
+
+.headline {
+    text-align: center;
+
+    margin: 96px 0px 64px 0px;
+
+    padding: 0px 20px;
+
+	font-family: 'Departure';
+	font-size: 2.0625rem;
+}
+
+.headline.mobile { 
+    margin: 1rem 0rem;
+}
+
+.accent {
+    color: var(--accent);
+
+    text-shadow: -2px 2px 0px var(--primary-lt);
+
+	font-size: 2.75rem
+}
+</style>
