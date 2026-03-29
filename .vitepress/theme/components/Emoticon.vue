050103084c5ebb129d546290c4f40b782e05570d
Liam Hanrahan <outercloudstudio@gmail.com>
2026-03-29 18:38:18 -0400

    add components

---
+++ .vitepress/theme/components/Emoticon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+    <span class="emoticon">
+        <slot />
+    </span>
+</template>
+
+<style scoped>
+.emoticon {
+    white-space: nowrap;
+    margin: 0px 2px;
+
+    font-family: 'jgs', Arial, Helvetica;
+}
+</style>
