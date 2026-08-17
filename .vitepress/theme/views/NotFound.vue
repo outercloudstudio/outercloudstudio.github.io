6254de7ad853da31d63be75ec7b8f7c8ae8f94d8
Liam Hanrahan <outercloudstudio@gmail.com>
2026-08-17 13:18:24 -0400

    add 404 easter egg

--- .vitepress/theme/views/NotFound.vue
+++ .vitepress/theme/views/NotFound.vue
@@ -5,7 +5,7 @@
         <h1 class="headline">How'd you get here?</h1>
 
         <main>
-            <p>There's been a 404 error. Here let's go <a href="/">back home.</a></p>
+            <p>There's been a 404 error. Dont worry, <a href="https://youtu.be/ntHQOPH7CaU?si=IFFeFzytKdEFb-s3" target="_blank">you can always come home!</a></p>
         </main>
     </div>
 </template>
